60be35b3dee851bb722cf49cbefcf75f89097c8f
HOSTEER <71406104+HOSTEER@users.noreply.github.com>
2023-09-30 15:24:37 +0200

    Update ecran strategie.c
    changement de couleur des boutons et amélioration de la compréhension du code

--- 3A/ecran_code_2324/src/strategie.c
+++ 3A/ecran_code_2324/src/strategie.c
@@ -1,9 +1,13 @@
 #include <aide.h>
 void score();
 
+#define BTN_NUMBER 6
+#define BTN_WIDTH 55
+#define BTN_HEIGHT 55
+
 static void event_btn(lv_event_t * e);
-static lv_style_t style_btn_green;
-static lv_style_t style_btn_blue;
+static lv_style_t btn_strat_orginal;
+static lv_style_t btn_strat_symetrique;
 
 void Style_btn_confirm();
 static lv_style_t style;
@@ -19,17 +23,17 @@ extern UART_HandleTypeDef huart1;
 void strategie()
 {
 	Style_btn_confirm();
-	lv_style_init(&style_btn_green);
-	lv_style_set_bg_color(&style_btn_green, lv_palette_main(LV_PALETTE_GREEN));
-	lv_style_set_radius(&style_btn_green, 80);
-	lv_style_set_width(&style_btn_green, 64);
-	lv_style_set_height(&style_btn_green, 55);
+	lv_style_init(&btn_strat_orginal);
+	lv_style_set_bg_color(&btn_strat_orginal, lv_palette_main(LV_PALETTE_YELLOW));
+	lv_style_set_radius(&btn_strat_orginal, 80);
+	lv_style_set_width(&btn_strat_orginal, BTN_WIDTH);
+	lv_style_set_height(&btn_strat_orginal, BTN_HEIGHT);
 
-	lv_style_init(&style_btn_blue);
-	lv_style_set_bg_color(&style_btn_blue, lv_palette_main(LV_PALETTE_BLUE));
-	lv_style_set_radius(&style_btn_blue, 80);
-    lv_style_set_width(&style_btn_blue, 64);
-    lv_style_set_height(&style_btn_blue, 55);
+	lv_style_init(&btn_strat_symetrique);
+	lv_style_set_bg_color(&btn_strat_symetrique, lv_palette_main(LV_PALETTE_BLUE));
+	lv_style_set_radius(&btn_strat_symetrique, 80);
+    lv_style_set_width(&btn_strat_symetrique, BTN_WIDTH);
+    lv_style_set_height(&btn_strat_symetrique, BTN_HEIGHT);
 
     LV_IMG_DECLARE(TableRedi);
     table = lv_img_create(lv_scr_act());
@@ -38,35 +42,37 @@ void strategie()
     lv_img_set_src(table, &TableRedi);
 
     uint32_t i;
-    for(i = 0; i < 6; i++) {
+    for(i = 0; i < BTN_NUMBER; i++) {
     	lv_obj_t * btn = lv_btn_create(table);
 	   	lv_obj_add_flag(btn, LV_OBJ_FLAG_EVENT_BUBBLE);
-
+	   	// valeur de i défini aussi le changement de couleur après un appuie
+	   	// i paire la couleur après un appuie sera verte alors que i impaire la
+	   	// la couleur sera beu
 	   	switch(i)
 	   	{
 	   	case 0:
-	   		lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 69-32, 30-27);
-	   		lv_obj_add_style(btn, &style_btn_blue, 0);
+	   		lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 413-(BTN_WIDTH/2), 30-(BTN_HEIGHT/2));
+	   		lv_obj_add_style(btn, &btn_strat_orginal, 0);
 	   		break;
 	    case 1:
-	       	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 411-32, 30-27);
-	       	lv_obj_add_style(btn, &style_btn_green, 0);
+	    	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 67-(BTN_WIDTH/2), 30-(BTN_HEIGHT/2));
+	       	lv_obj_add_style(btn, &btn_strat_symetrique, 0);
 	       	break;
 	    case 2:
-	    	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 411-32, 135-27);
-	       	lv_obj_add_style(btn, &style_btn_blue, 0);
+	    	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 67-(BTN_WIDTH/2), 135-(BTN_HEIGHT/2));
+	       	lv_obj_add_style(btn, &btn_strat_orginal, 0);
 	      	break;
 	    case 3:
-	       	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 69-32, 135-27);
-        	lv_obj_add_style(btn, &style_btn_green, 0);
+	    	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 413-(BTN_WIDTH/2), 135-(BTN_HEIGHT/2));
+        	lv_obj_add_style(btn, &btn_strat_symetrique, 0);
         	break;
 	    case 4:
-	       	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 411-32, 241-27);
-	       	lv_obj_add_style(btn, &style_btn_green, 0);
+	       	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 413-(BTN_WIDTH/2), 241-(BTN_HEIGHT/2));
+	       	lv_obj_add_style(btn, &btn_strat_orginal, 0);
 	       	break;
 	    case 5:
-	       	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 69-32, 241-27);
-	       	lv_obj_add_style(btn, &style_btn_blue, 0);
+	       	lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 67-(BTN_WIDTH/2), 241-(BTN_HEIGHT/2));
+	       	lv_obj_add_style(btn, &btn_strat_symetrique, 0);
 	       	break;
 	    default:
 	    	break;
@@ -146,12 +152,12 @@ static void event_btn(lv_event_t * e)
     if(target == cont) return;
 
     Index = lv_obj_get_index(target);
-    if(confirm == 0 && Index == 10)
+    if(confirm == 0 && Index == BTN_NUMBER)
     {
     	confirm++;
-    	lv_color_t r = lv_palette_main(LV_PALETTE_RED);
+    	lv_color_t r = lv_palette_main(LV_PALETTE_NONE);
     	fullr = r.full;
-    	for(int i = 0;i<10;i++)
+    	for(int i = 0;i<BTN_NUMBER;i++)
     	{
     		lv_obj_t * obj = lv_obj_get_child(cont, i);
     		lv_color_t color = lv_obj_get_style_bg_color(obj, LV_PART_MAIN);
@@ -169,7 +175,7 @@ static void event_btn(lv_event_t * e)
 
     		if(i%2 == 0)
     		{
-    			lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_GREEN), 0);
+    			lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_YELLOW), 0);
     		}
     		else
     		{
@@ -184,14 +190,14 @@ static void event_btn(lv_event_t * e)
     }
     else if(confirm == 0 && Index != 10)
     {
-    	for(int i = 0;i<10;i++)
+    	for(int i = 0;i<BTN_NUMBER;i++)
     	{
     		lv_obj_t * obj = lv_obj_get_child(cont, i);
     	    if(obj != target)
     	    {
     	    	if(i%2 == 0)
     	    	{
-    	    		lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_GREEN), 0);
+    	    		lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_YELLOW), 0);
     	    	}
     	    	else
     	    	{
@@ -200,7 +206,7 @@ static void event_btn(lv_event_t * e)
     	    }
     	    else
     	    {
-    	    	lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_RED), 0);
+    	    	lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_NONE), 0);
     	    }
     	}
     }
