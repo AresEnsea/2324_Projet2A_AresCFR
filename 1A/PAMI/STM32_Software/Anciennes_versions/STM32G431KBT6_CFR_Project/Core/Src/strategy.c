bb6d065c3531fb2b96fac1e783dd468030ecabe2
Carpe-bleue <88663397+Carpe-bleue@users.noreply.github.com>
2024-05-28 14:11:33 +0200

    Update strategy.c

--- 1A/PAMI/STM32_Software/Anciennes_versions/STM32G431KBT6_CFR_Project/Core/Src/strategy.c
+++ 1A/PAMI/STM32_Software/Anciennes_versions/STM32G431KBT6_CFR_Project/Core/Src/strategy.c
@@ -28,53 +28,141 @@ void STRATEGY_Init(void){
 	strategies[0].action[0].speed_Motor_Right = 0;
 	strategies[0].action[0].enable_stop_lidar = 1;
 
-	// STRATEGY BLUE 1
+	// STRATEGY BLUE 1  (CLOSEST ZONE RIGHT SIDE)
 	strategies[1].id = B1_Strat;
 	strategies[1].name[0] = 'B';
 	strategies[1].name[1] = '1';
 	strategies[1].action_id = 0;
-	strategies[1].action_nb = 4;
-	strategies[1].action[0].start_Time = 0;
-	strategies[1].action[0].speed_Motor_Left = 0;
-	strategies[1].action[0].speed_Motor_Right = 0;
+	strategies[1].action_nb = 10;
+    strategies[1].action[0].start_Time = 0;
+	strategies[1].action[0].speed_Motor_Left = 5;
+	strategies[1].action[0].speed_Motor_Right = 10;
 	strategies[1].action[0].enable_stop_lidar = 1;
 
 	strategies[1].action[1].start_Time = 50;
-	strategies[1].action[1].speed_Motor_Left = 10;
+	strategies[1].action[1].speed_Motor_Left = 5;
 	strategies[1].action[1].speed_Motor_Right = 10;
 	strategies[1].action[1].enable_stop_lidar = 1;
 
 	strategies[1].action[2].start_Time = 100;
-	strategies[1].action[2].speed_Motor_Left = 0;
+	strategies[1].action[2].speed_Motor_Left = 5;
 	strategies[1].action[2].speed_Motor_Right = 10;
 	strategies[1].action[2].enable_stop_lidar = 1;
 
 	strategies[1].action[3].start_Time = 150;
-	strategies[1].action[3].speed_Motor_Left = 0;
-	strategies[1].action[3].speed_Motor_Right = 0;
+	strategies[1].action[3].speed_Motor_Left = 5;
+	strategies[1].action[3].speed_Motor_Right = 10;
 	strategies[1].action[3].enable_stop_lidar = 1;
 
-	// STRATEGY BLUE 2
+	strategies[1].action[4].start_Time = 200;
+	strategies[1].action[4].speed_Motor_Left = 10;
+	strategies[1].action[4].speed_Motor_Right = 10;
+	strategies[1].action[4].enable_stop_lidar = 1;
+
+	strategies[1].action[5].start_Time = 250;
+	strategies[1].action[5].speed_Motor_Left = 10;
+	strategies[1].action[5].speed_Motor_Right = 10;
+	strategies[1].action[5].enable_stop_lidar = 1;
+
+	strategies[1].action[6].start_Time = 300;
+	strategies[1].action[6].speed_Motor_Left = 10;
+	strategies[1].action[6].speed_Motor_Right = 10;
+	strategies[1].action[6].enable_stop_lidar = 1;
+
+	strategies[1].action[7].start_Time = 350;
+	strategies[1].action[7].speed_Motor_Left = 10;
+	strategies[1].action[7].speed_Motor_Right = 10;
+	strategies[1].action[7].enable_stop_lidar = 1;
+
+	strategies[1].action[8].start_Time = 400;
+	strategies[1].action[8].speed_Motor_Left = 10;
+	strategies[1].action[8].speed_Motor_Right = 10;
+	strategies[1].action[8].enable_stop_lidar = 1;
+
+	strategies[1].action[9].start_Time = 450;
+	strategies[1].action[9].speed_Motor_Left = 10;
+	strategies[1].action[9].speed_Motor_Right = 10;
+	strategies[1].action[9].enable_stop_lidar = 1;
+
+
+
+
+	// STRATEGY BLUE 2		(SECOND ZONE ON RIGHT SIDE)
 	strategies[2].id = B2_Strat;
 	strategies[2].name[0] = 'B';
 	strategies[2].name[1] = '2';
 	strategies[2].action_id = 0;
-	strategies[2].action_nb = 1;
-	strategies[2].action[0].start_Time = 0;
-	strategies[2].action[0].speed_Motor_Left = 0;
-	strategies[2].action[0].speed_Motor_Right = 0;
+	strategies[2].action_nb = 6;
+
+	strategies[2].action[0].start_Time = 0;				//LIDAR SETUP
+	strategies[2].action[0].speed_Motor_Left = 10;
+	strategies[2].action[0].speed_Motor_Right = 10;
 	strategies[2].action[0].enable_stop_lidar = 1;
 
+	strategies[2].action[1].start_Time = 50;			// MOVE FORWARD
+	strategies[2].action[1].speed_Motor_Left = 10;
+	strategies[2].action[1].speed_Motor_Right = 10;
+	strategies[2].action[1].enable_stop_lidar = 1;
+
+	strategies[2].action[2].start_Time = 100;			// TURN RIGHT
+	strategies[2].action[2].speed_Motor_Left = 10;
+	strategies[2].action[2].speed_Motor_Right = 10;
+	strategies[2].action[2].enable_stop_lidar = 1;
+
+	strategies[2].action[3].start_Time = 150;			// MOVE FORWARD W LIDAR
+	strategies[2].action[3].speed_Motor_Left = 10;
+	strategies[2].action[3].speed_Motor_Right = 10;
+	strategies[2].action[3].enable_stop_lidar = 1;
+
+	strategies[2].action[4].start_Time =200;			// CONTINUE SLOWER WITHOUT LIDAR
+	strategies[2].action[4].speed_Motor_Left = 5;
+	strategies[2].action[4].speed_Motor_Right = 5;
+	strategies[2].action[4].enable_stop_lidar = 1;
+
+	strategies[2].action[5].start_Time = 250;			// STOP
+	strategies[2].action[5].speed_Motor_Left = 10;
+	strategies[2].action[5].speed_Motor_Right = 10;
+	strategies[2].action[5].enable_stop_lidar = 1;
+
+	// STRATEGY BLUE 3	(MIDDLE LEFT ZONE)
 	strategies[3].id = B3_Strat;
 	strategies[3].name[0] = 'B';
 	strategies[3].name[1] = '3';
 	strategies[3].action_id = 0;
-	strategies[3].action_nb = 1;
-	strategies[3].action[0].start_Time = 0;
+	strategies[3].action_nb = 6;
+
+	strategies[3].action[0].start_Time = 0;				//LIDAR SETUP
 	strategies[3].action[0].speed_Motor_Left = 0;
 	strategies[3].action[0].speed_Motor_Right = 0;
 	strategies[3].action[0].enable_stop_lidar = 1;
 
+	strategies[3].action[1].start_Time = 50;			// MOVE FORWARD
+	strategies[3].action[1].speed_Motor_Left = 10;
+	strategies[3].action[1].speed_Motor_Right = 10;
+	strategies[3].action[1].enable_stop_lidar = 1;
+
+	strategies[3].action[2].start_Time = 3000;			// TURN LEFT
+	strategies[3].action[2].speed_Motor_Left = 0;
+	strategies[3].action[2].speed_Motor_Right = 10;
+	strategies[3].action[2].enable_stop_lidar = 1;
+
+	strategies[3].action[3].start_Time = 4500;			// MOVE FORWARD W LIDAR
+	strategies[3].action[3].speed_Motor_Left = 10;
+	strategies[3].action[3].speed_Motor_Right = 10;
+	strategies[3].action[3].enable_stop_lidar = 1;
+
+	strategies[3].action[4].start_Time = 8000;			// CONTINUE SLOWER WITHOUT LIDAR
+	strategies[3].action[4].speed_Motor_Left = 5;
+	strategies[3].action[4].speed_Motor_Right = 5;
+	strategies[3].action[4].enable_stop_lidar = 0;
+
+	strategies[3].action[5].start_Time = 9000;			// STOP
+	strategies[3].action[5].speed_Motor_Left = 0;
+	strategies[3].action[5].speed_Motor_Right = 0;
+	strategies[3].action[5].enable_stop_lidar = 0;
+
+
+	// STRATEGY JAUNE 1
 	strategies[4].id = J1_Strat;
 	strategies[4].name[0] = 'J';
 	strategies[4].name[1] = '1';
@@ -85,6 +173,7 @@ void STRATEGY_Init(void){
 	strategies[4].action[0].speed_Motor_Right = 0;
 	strategies[4].action[0].enable_stop_lidar = 1;
 
+	// STRATEGY JAUNE 2
 	strategies[5].id = J2_Strat;
 	strategies[5].name[0] = 'J';
 	strategies[5].name[1] = '2';
@@ -95,6 +184,7 @@ void STRATEGY_Init(void){
 	strategies[5].action[0].speed_Motor_Right = 0;
 	strategies[5].action[0].enable_stop_lidar = 1;
 
+	// STRATEGY JAUNE 3
 	strategies[6].id = J3_Strat;
 	strategies[6].name[0] = 'J';
 	strategies[6].name[1] = '3';
